db062a0bec1e1429ca294bb958aaf2419ff10fbd
ahmed077 <don.zero23@gmail.com>
2018-05-08 23:06:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// var x = {
+//     firstName: 'A',
+//     lastName: 'M',
+//     calcAge: function(n) {
+//         return n * 365;
+//     }
+// };
+// console.log(x.firstName);
+// console.log(x.calcAge(21));
+// //>> 'A'
+// //>> '21 * 365'
+// var y = ['A','M'];
+// console.log(y[0]);
+// //>> 'A'
+// var z = ['A', ['C', 'D'] ];
+//
+// /*
+// * $x = ['A','M'];
+// * $y = [
+// * 'firstName' => 'A',
+// * 'lastName' => 'M'
+// * ]
+// * $y['lastName']  >> 'M'
+// *
+// * Class Test {
+// * $firstName->'a';
+// *
+// * }
+// * Test->firstName
+// *
+// * */
